ed45645b357ecc040665da26f0170015719a68e9
Mads Johansen <maxi_johansen@hotmail.com>
2026-04-28 13:43:11 +0200

    product

---
+++ src/Product.java
@@ -0,0 +1,9 @@
+public class Product {
+    private String name;
+    private double price;
+    private boolean inStock;
+
+    public Product(String name, double price, boolean inStock){
+
+    }
+}
